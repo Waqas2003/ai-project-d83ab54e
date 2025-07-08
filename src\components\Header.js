497b7b0f0bee26a8f7b842fe66326a5e7690f1ad
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 00:58:24 +0500

    Add src\components\Header.js

---
+++ src\components\Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Portugal Football Team Logo" />
+      <h1>Portugal Football Team</h1>
+    </header>
+  );
+};
+
+export default Header;
+```
+**src/components/Hero.js:**
