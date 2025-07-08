78d1fc8548de931edbd26cc627c7ebd4b41d5c5e
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 00:58:26 +0500

    Add src\components\Hero.js

---
+++ src\components\Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h2>European Champions 2016</h2>
+      <p>Portugal's national football team has a rich history of success...</p>
+      <button>Learn More</button>
+    </section>
+  );
+};
+
+export default Hero;
+```
+**src/components/Players.js:**
