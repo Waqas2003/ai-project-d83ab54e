cbd4e5c4c16a89221f8c7769ea2d8803e189b8f2
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 00:58:30 +0500

    Add src\containers\App.js

---
+++ src\containers\App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Players from '../components/Players';
+import Footer from '../components/Footer';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Hero />
+      <Players />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
+```
+**src/index.js:**
