e029fdb07b9a5440642ab9a0a33020c7d84b9ab9
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 00:58:23 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Portugal Football Team</p>
+    </footer>
+  );
+};
+
+export default Footer;
+```
+**src/containers/App.js:**
